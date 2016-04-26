5497faa753087910e927a7b127aa2a1e851399e9
Jason Vanderslice <jlvanderslice@gmail.com>
2016-04-26 12:07:06 -0500

    Begun file/folder structure for boilerplate

---
+++ js/main.js
@@ -0,0 +1,7 @@
+(function() {
+  // All code NOT referencing DOM elements can go here
+  document.addEventLister("DOMContentLoaded", function(e){
+    // ALL DOM RELATED QUERYING GOES HERE
+  });
+
+}());
