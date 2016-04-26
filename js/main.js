506cf4cf9dd928600950ee2636f03ce83587132c
Jason Vanderslice <jlvanderslice@gmail.com>
2016-04-26 13:42:51 -0500

    Fixed spelling error for adding event listener

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 (function() {
   // All code NOT referencing DOM elements can go here
-  document.addEventLister("DOMContentLoaded", function(e){
+  document.addEventListener("DOMContentLoaded", function(e){
     // ALL DOM RELATED QUERYING GOES HERE
   });
 
